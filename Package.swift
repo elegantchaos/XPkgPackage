a160d269f2944e61335847cb9d72ab53269d8baa
Sam Deane <sam@elegantchaos.com>
2022-07-19 14:35:09 +0100

    Tweaked formatting

--- Package.swift
+++ Package.swift
@@ -4,14 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "XPkgPackage",
+    
     platforms: [
         .macOS(.v10_13)
     ],
+    
     products: [
         .library(
             name: "XPkgPackage",
-            targets: ["XPkgPackage"]),
+            targets: ["XPkgPackage"]
+        ),
     ],
+
     dependencies: [
         .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.7.4"),
         .package(url: "https://github.com/elegantchaos/Runner.git", from: "1.3.2"),
@@ -21,12 +25,20 @@ let package = Package(
         .package(url: "https://github.com/elegantchaos/SwiftFormatterPlugin.git", from: "1.0.2")
 
     ],
+    
     targets: [
         .target(
             name: "XPkgPackage",
-            dependencies: ["Runner", "Logger"]),
+            dependencies: ["Runner", "Logger"]
+        ),
+        
         .testTarget(
             name: "XPkgPackageTests",
-            dependencies: ["XPkgPackage"]),
+            dependencies: ["XPkgPackage"]
+        ),
+    ],
+    
+    swiftLanguageVersions: [
+        .v5, .v
     ]
 )
